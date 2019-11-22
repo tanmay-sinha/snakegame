f5bedf49781935d63e16dbc08d46d513dfe2a15f
Tanmay Sinha <alokratn001@gmail.com>
2019-11-22 15:20:56 +0530

    fixed bugs and added new feature and background images

--- app.py
+++ app.py
@@ -7,21 +7,27 @@ pygame.mixer.pre_init(44100, 16, 2, 4096)
 pygame.init()
 
 sounds = []
-sounds.append(pygame.mixer.Sound(r'D:/gameproj/music/eat.wav'))
+sounds.append(pygame.mixer.Sound(r'music/eat.wav'))
 
 white = (255, 255, 255)
 red = (255, 0, 0)
 black = (0, 0, 0)
-yellow = (255,255,0)
+yellow = (0,255,0)
 screen_width = 900
 screen_height = 700
 gameWindow = pygame.display.set_mode((screen_width,screen_height))
+
+bg1 = pygame.image.load(r"images/welcome.png")
+bg1 = pygame.transform.scale(bg1, (screen_width, screen_height)).convert_alpha()
+bg2 = pygame.image.load(r"images/game.jpg")
+bg2 = pygame.transform.scale(bg2, (screen_width, screen_height)).convert_alpha()
+bg3 = pygame.image.load(r"images/gameover.jpg")
+bg3 = pygame.transform.scale(bg3, (screen_width, screen_height)).convert_alpha()
 
 pygame.display.set_caption("SAANP KA KHEL")
 pygame.display.update()
 clock = pygame.time.Clock()
 font = pygame.font.SysFont(None, 40)
-
 
 def text_score(text, color, x, y):
     screen_text = font.render(text,True,color)
@@ -31,15 +37,27 @@ def plot_snk(gameWindow,color,snk_list,snake_size):
     for x,y in snk_list:
         pygame.draw.rect(gameWindow,color,[x,y,snake_size,snake_size])
 
+def gameov(score,hscore,snk_list,snake_size,food_x,food_y):
+    for i in range(0,7):
+        #gameWindow.fill(white)
+        gameWindow.blit(bg2,(0,0))
+        pygame.draw.rect(gameWindow, red, [food_x, food_y, snake_size + 5, snake_size + 5])
+        text_score("Score: " + str(score) + "    High Score: " + str(hscore), red, 2.5, 2.5)
+        if i%2==0:
+            plot_snk(gameWindow, black, snk_list, snake_size)
+        pygame.display.update()
+        pygame.time.delay(800)
+
 def welcome():
     exit_game = False
     pygame.mixer.music.load(r'music/welcome.mp3')
     pygame.mixer.music.set_volume(0.5)
     pygame.mixer.music.play(start=32.0)
     while not exit_game:
-        gameWindow.fill((233,220,229))
-        text_score("Welcome to Snakes!!! ",black,300,300)
-        text_score("Press Space Bar to play.. ",black,280,350)
+        #gameWindow.fill((233,220,229))
+        gameWindow.blit(bg1,(0,0))
+        text_score("Welcome to Snakes!!! ",yellow,300,300)
+        text_score("Press Space Bar to play.. ",yellow,280,350)
         text_score("Press BACKSPACE to quit..", red, 270, 400)
 
         for event in pygame.event.get():
@@ -59,12 +77,12 @@ def welcome():
 def gameloop():
     exit_game = False
     game_over = False
-    snake_x = 45
-    snake_y = 55
+    snake_x = 200
+    snake_y = 150
     velocity_x = 0
     velocity_y = 0
-    v_init = 0.5
-    snk_list = []
+    snk_list =[]
+    v_init = 1
     snk_length = 1
     if(not os.path.exists("highscore.txt")):
         with open("highscore.txt","w") as f:
@@ -75,7 +93,7 @@ def gameloop():
     food_x = random.randint(40, screen_width / 2)
     food_y = random.randint(40, screen_height / 2)
 
-    snake_size = 30
+    snake_size = 25
     fps = 60
     score = 0
 
@@ -83,7 +101,9 @@ def gameloop():
         if game_over:
             with open("highscore.txt", "w") as f:
                 f.write(str(hscore))
-            gameWindow.fill((233,229,220))
+            #gameWindow.fill((233,229,220))
+            gameWindow.blit(bg3,(0,0))
+            text_score("Your Score : "+str(score),yellow,380,300)
             text_score("Game Over, Press ENTER to play again!",red,200,350)
             text_score("Press BACKSPACE to quit..",red,300,400)
 
@@ -123,15 +143,16 @@ def gameloop():
             if abs(snake_x-food_x)<15 and abs(snake_y-food_y)<15:
                 sounds[0].play()
                 score += 10
-                v_init += 0.05
+                v_init += 0.1
                 food_x = random.randint(20, screen_width / 2)
                 food_y = random.randint(20, screen_height / 2)
                 snk_length += 20
                 if score > int(hscore):
                     hscore = score
-            gameWindow.fill(white)
+            #gameWindow.fill(white)
+            gameWindow.blit(bg2,(0,0))
             text_score("Score: " + str(score)+ "    High Score: "+str(hscore), red, 2.5, 2.5)
-            pygame.draw.rect(gameWindow,red,[food_x,food_y,snake_size,snake_size])
+            pygame.draw.rect(gameWindow,red,[food_x, food_y, snake_size+5, snake_size+5])
 
             head = []
             head.append(snake_x)
@@ -142,16 +163,18 @@ def gameloop():
                 del snk_list[0]
 
             if head in snk_list[:-1]:
-                game_over = True
                 pygame.mixer.music.load(r'music/over.mp3')
                 pygame.mixer.music.play()
+                gameov(score,hscore,snk_list,snake_size,food_x,food_y)
+                game_over = True
+
 
             if snake_x<0 or snake_x>screen_width or snake_y<0 or snake_y>screen_height:
-                game_over = True
                 pygame.mixer.music.load(r'music/over.mp3')
                 pygame.mixer.music.play()
+                gameov(score,hscore,snk_list,snake_size,food_x,food_y)
+                game_over = True
 
-            pygame.draw.rect(gameWindow,black,[snake_x,snake_y,snake_size,snake_size])
             plot_snk(gameWindow,black,snk_list,snake_size)
 
         pygame.display.update()
